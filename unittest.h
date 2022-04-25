be7c96d8c14c0d8d302d711a945c95bf49c09075
Arjob Mukherjee <mukherjeearjob@gmail.com>
2022-04-26 02:51:42 +0530

    Added include guard

--- unittest.h
+++ unittest.h
@@ -6,7 +6,12 @@
  * Dated : 26 April 2022
  * ------------------------------------------------------------------------------------------------
  */
+#ifndef UNITTEST_H
+#define UNITTEST_H
+
 #define CHECK(t) (t) ? printf ("%s : PASS\n", __func__) \
                      : printf ("%s : FAIL (%s)\n", __func__, #t)
 #define EQ(a,b)  CHECK(a==b)
 #define NEQ(a,b) CHECK(a!=b)
+
+#endif // UNITTEST_H
