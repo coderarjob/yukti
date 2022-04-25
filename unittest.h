8cb184c1c1d796974b7d7c5810f5e82126e0ac31
Arjob Mukherjee <mukherjeearjob@gmail.com>
2022-04-26 01:53:15 +0530

    Added file with macros for equality checks.
    * When equality fails, either PASS or FAIL is printed on the terminal.

---
+++ unittest.h
@@ -0,0 +1,12 @@
+/*
+ * ------------------------------------------------------------------------------------------------
+ * Macros for equality and negation checks.
+ *
+ * Author: arjobmukherjee@gmail.com (Arjob Mukherjee)
+ * Dated : 26 April 2022
+ * ------------------------------------------------------------------------------------------------
+ */
+#define CHECK(t) (t) ? printf ("%s : PASS\n", __func__) \
+                     : printf ("%s : FAIL (%s)\n", __func__, #t)
+#define EQ(a,b)  CHECK(a==b)
+#define NEQ(a,b) CHECK(a!=b)
