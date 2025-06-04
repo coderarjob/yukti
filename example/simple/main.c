b64f7e5d0388c212dcad4d499f383b01743e8b01
Arjob Mukherjee <arjobmukherjee@gmail.com>
2025-06-05 02:34:39 +0530

    Updated example to use  YT_MUST_CALL_IN_ORDER
    Also includes:
    * Code formatting changes

--- example/simple/main.c
+++ example/simple/main.c
@@ -13,33 +13,35 @@ int fn_under_test (bool m)
 {
     if (m) {
         // Branch 1
+        fnB (11, 'b', 13);
         return fnA();
     } else {
         // Branch 2
-        fnB(10,'a',13);
+        fnB (10, 'a', 13);
         return 1;
     }
 }
 
-YT_TEST(simple, branch_1_test)
+YT_TEST (simple, branch_1_test)
 {
     fnA_fake.ret = 10;
-    YT_EQ_SCALAR(fn_under_test(true), 10);
-    YT_MUST_CALL_ANY_ORDER(fnA);
+    YT_EQ_SCALAR (fn_under_test (true), 10);
+    YT_MUST_CALL_IN_ORDER (fnB, _, YT_V ('b'), _);
+    YT_MUST_CALL_IN_ORDER (fnA);
     YT_END();
 }
 
-YT_TEST(simple, branch_2_test)
+YT_TEST (simple, branch_2_test)
 {
-    YT_EQ_SCALAR(fn_under_test(false), 1);
-    YT_MUST_CALL_ANY_ORDER(fnB,YT_V(10),YT_V('a'),_);
+    YT_EQ_SCALAR (fn_under_test (false), 1);
+    YT_MUST_CALL_ANY_ORDER (fnB, YT_V (10), YT_V ('a'), _);
     YT_END();
 }
 
 void reset()
 {
-    YT_RESET_MOCK(fnA);
-    YT_RESET_MOCK(fnB);
+    YT_RESET_MOCK (fnA);
+    YT_RESET_MOCK (fnB);
 }
 
 int main()
