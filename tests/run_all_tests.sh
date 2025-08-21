6059f00cb612465ff5e3b0fe867c08355c3e92ea
Arjob Mukherjee <arjobmukherjee@gmail.com>
2025-08-21 16:56:24 +0530

    Adds 32-bit (i386) compilation of integration tests
    This was added to catch errors/warning when compiled with '-m32'
    compiler option. This also catches arch dependent differences in the
    tests and yukti.h.

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -3,12 +3,21 @@
 TESTS_DIR=`dirname $0`
 
 #######################################################################
-# Compiling and running tests with GCC
+# Compiling and running tests with GCC (Native)
 #######################################################################
 export CC=gcc
 export CFLAGS="-Wall -Wextra -std=c99 -DYT__TESTING"
 
-echo "Test 1/2: Running tests with C Compiler ($CC)"
+echo "Test 1/2: Running tests with C Compiler ($CC) (Native)"
+bash $TESTS_DIR/test.sh || exit
+
+#######################################################################
+# Compiling and running tests with GCC (m32)
+#######################################################################
+export CC=gcc
+export CFLAGS="-Wall -Wextra -std=c99 -m32 -DYT__TESTING"
+
+echo "Test 2/3: Running tests with C Compiler ($CC) (32-bits)"
 bash $TESTS_DIR/test.sh || exit
 
 #######################################################################
@@ -17,7 +26,7 @@ bash $TESTS_DIR/test.sh || exit
 export CC=g++
 export CFLAGS="-Wall -Wextra -DYT__TESTING"
 
-echo "Test 2/2: Running tests with C++ Compiler ($CC)"
+echo "Test 3/3: Running tests with C++ Compiler ($CC)"
 bash $TESTS_DIR/test.sh || exit
 
 echo "[All tests passed]"
